d01f4324cccbcd1f96995938acafd9cb4547e701
Philippe Plagnol <philippe.plagnol@gmail.com>
2016-02-23 14:43:51 +0800

    Heroku - config

--- opd/settings.py
+++ opd/settings.py
@@ -145,9 +145,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-#STATICFILES_DIRS = (
-#    os.path.join(PROJECT_ROOT, 'static'),
-#)
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
