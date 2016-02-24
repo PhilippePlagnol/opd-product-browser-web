cdf95eb08c4708a6eb6c50b4c5e445b2cac1e132
Philippe Plagnol <philippe.plagnol@gmail.com>
2016-02-24 16:10:27 +0800

    Static root

--- opd/settings.py
+++ opd/settings.py
@@ -146,13 +146,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
-#STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
